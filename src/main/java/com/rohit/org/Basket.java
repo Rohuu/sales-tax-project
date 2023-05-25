cdf70e5cf8e4aecf7c8b75d9a6ee9d2fe162b45c
Rohuu <rohuu99022@gmail.com>
2023-05-25 11:17:35 +0530

    functionality: add a basketItem to basket

--- src/main/java/com/rohit/org/Basket.java
+++ src/main/java/com/rohit/org/Basket.java
@@ -1,4 +1,17 @@
 package com.rohit.org;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Basket {
+    List<BasketItem> basketItems=new ArrayList<>();
+
+    public void addBasketItem(BasketItem basketItem) {
+        basketItems.add(basketItem);
+    }
+
+    public int size() {
+        return basketItems.size();
+    }
 }
--- src/test/java/com/rohit/org/BasketTest.java
+++ src/test/java/com/rohit/org/BasketTest.java
@@ -3,6 +3,8 @@ package com.rohit.org;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 public class BasketTest {
 
     @Test
@@ -10,5 +12,15 @@ public class BasketTest {
         Basket basket = new Basket();
 
         Assertions.assertNotNull(basket);
+    }
+
+    @Test
+    public void shouldBeAbleToAddBasketItem(){
+        Basket basket=new Basket();
+        Product product = new Product("Book", Category.BOOK);
+        BasketItem basketItem = new BasketItem(product,1, BigDecimal.valueOf(50));
+        basket.addBasketItem(basketItem);
+
+        Assertions.assertEquals(1,basket.size());
     }
 }
