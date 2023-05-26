f9fa7809fccfb118503f96b8ad3776da10354060
Rohuu <rohuu99022@gmail.com>
2023-05-26 15:25:37 +0530

    created ReceiptItem
    to store input details from ReceiptItemParser

---
+++ src/main/java/com/rohit/org/ReceiptItem.java
@@ -0,0 +1,43 @@
+package com.rohit.org;
+
+import java.math.BigDecimal;
+
+public class ReceiptItem {
+    private final String productName;
+    private final boolean isImported;
+    private final int quantity;
+    private final BigDecimal unitPrice;
+
+    public ReceiptItem(String productName, boolean isImported, int quantity, BigDecimal unitPrice) {
+        this.productName = productName;
+        this.isImported = isImported;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public boolean isImported() {
+        return isImported;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "ReceiptItem{" +
+                "productName='" + productName + '\'' +
+                ", isImported=" + isImported +
+                ", quantity=" + quantity +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+}
