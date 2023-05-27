3374566f042b616a550032b7bf3604d924cb0e31
Rohuu <rohuu99022@gmail.com>
2023-05-27 14:20:12 +0530

    added tests for ReceiptPrinter

--- src/main/java/com/rohit/org/ReceiptPrinter.java
+++ src/main/java/com/rohit/org/ReceiptPrinter.java
@@ -5,36 +5,43 @@ import java.util.List;
 
 public class ReceiptPrinter {
 
-    BigDecimal totalSalesTax=BigDecimal.ZERO;
-    BigDecimal totalAmount=BigDecimal.ZERO;
+    BigDecimal totalSalesTax = BigDecimal.ZERO;
+    BigDecimal totalAmount = BigDecimal.ZERO;
 
 
-    public void printReceipt(Basket basket){
-       List<BasketItem> basketItems = basket.getBasketItems();
+    public String generateReceipt(Basket basket) {
+        List<BasketItem> basketItems = basket.getBasketItems();
+        String output = "";
 
-        for (var basketItem:basketItems){
-            Product product=basketItem.getProduct();
+        for (var basketItem : basketItems) {
+            Product product = basketItem.getProduct();
             String isImported = imported(product);
+
             // considering quantity 1 for all
-            BigDecimal totalPrice=   basketItem.getPrice().add(basketItem.getTaxPrice());
-            System.out.println(basketItem.getQuantity()+" "+isImported+product.getName()+": "+ totalPrice);
-            totalSalesTax=totalSalesTax.add(basketItem.getTaxPrice());
-            totalAmount=totalAmount.add(totalPrice);
+            BigDecimal totalPrice = basketItem.getPrice().add(basketItem.getTaxPrice());
+            totalSalesTax = totalSalesTax.add(basketItem.getTaxPrice());
+            totalAmount = totalAmount.add(totalPrice);
+            String line = basketItem.getQuantity() + " " + isImported + product.getName() + ": " + totalPrice;
+            line = line + '\n';
+            output = output + line;
         }
-        printSalesTaxAndTotalAmount(totalSalesTax,totalAmount);
+        output = output + generateSalesTaxAndTotalAmount(totalSalesTax, totalAmount);
+        return output;
     }
 
     private String imported(Product product) {
         String imported;
-        if(product.isImported())
+        if (product.isImported())
             imported = "imported ";
         else
-            imported="";
+            imported = "";
         return imported;
     }
 
-    private static void printSalesTaxAndTotalAmount(BigDecimal totalSalesTax, BigDecimal totalAmount) {
-        System.out.println("Sales Taxes: "+totalSalesTax);
-        System.out.println("Total: "+totalAmount);
+    public String generateSalesTaxAndTotalAmount(BigDecimal totalSalesTax, BigDecimal totalAmount) {
+        String taxAndTotalAmount;
+        taxAndTotalAmount = "Sales Taxes: " + totalSalesTax + '\n';
+        taxAndTotalAmount = taxAndTotalAmount + "Total: " + totalAmount + '\n';
+        return taxAndTotalAmount;
     }
 }
--- src/test/java/com/rohit/org/ReceiptPrinterTest.java
+++ src/test/java/com/rohit/org/ReceiptPrinterTest.java
@@ -3,11 +3,46 @@ package com.rohit.org;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 public class ReceiptPrinterTest {
 
     @Test
     public void shouldBeAbleToCreateReceiptPrinter(){
         ReceiptPrinter receiptPrinter = new ReceiptPrinter();
         Assertions.assertNotNull(receiptPrinter);
+    }
+
+    @Test
+    public void shouldBeAbleToPrintReceipt(){
+        ReceiptPrinter receiptPrinter=new ReceiptPrinter();
+        Basket basket=new Basket();
+
+        Product product=new Product("book",Category.BOOK,false, BigDecimal.valueOf(100));
+        basket.addBasketItem(new BasketItem(product,1,BigDecimal.valueOf(100),BigDecimal.ZERO));
+        String expected= """
+                1 book: 100
+                Sales Taxes: 0
+                Total: 100
+                """;
+        String actual= receiptPrinter.generateReceipt(basket);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBeAbleToCalculateTaxAndTotalAmount(){
+        ReceiptPrinter receiptPrinter=new ReceiptPrinter();
+        Basket basket=new Basket();
+        Product product=new Product("book",Category.BOOK,false, BigDecimal.valueOf(100));
+        basket.addBasketItem(new BasketItem(product,1,BigDecimal.valueOf(100),BigDecimal.ZERO));
+
+        String expected= """
+                Sales Taxes: 0
+                Total: 100
+                """;
+        String actual= receiptPrinter.generateSalesTaxAndTotalAmount(BigDecimal.valueOf(0),BigDecimal.valueOf(100));
+
+        Assertions.assertEquals(expected, actual);
     }
 }
